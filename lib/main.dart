44d4ebe8e982652586a0ded6cbd6ab63a124c71f
Surya Dev Singh <46717624+suryadevsingh@users.noreply.github.com>
2019-10-21 21:09:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'tflite real-time detection',
+      title: "tflite real-time detection",
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
